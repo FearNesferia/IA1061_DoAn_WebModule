3e660f5a22e280c96e859c211fa465575bc204af
TIENHBSE61862\FearNesferia <fearnesferia@gmail.com>
2017-11-21 17:07:53 +0700

    xzc
    zxc

--- WebModule/WebModule/IISModule.cs
+++ WebModule/WebModule/IISModule.cs
@@ -1,13 +1,63 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using WebModule.Models;
 
 namespace WebModule
 {
-    public class IISModule
+    public class IISModule : IHttpModule
     {
+        private HttpClient client;
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
 
+        public void Init(HttpApplication context)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://fearnesferia.ddns.net/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var wrapper = new EventHandlerTaskAsyncHelper(SendRequest);
+            context.AddOnPreRequestHandlerExecuteAsync(wrapper.BeginEventHandler, wrapper.EndEventHandler);
+        }
+
+
+        private async Task SendRequest(Object sender, EventArgs taget)
+        {
+            HttpApplication application = (HttpApplication)sender;
+            HttpContext context = application.Context;
+            HttpRequest request = context.Request;
+            string path = request.Path;
+            string queryString = request.Url.Query;
+            string payload;
+            using (Stream receiveStream = request.InputStream)
+            {
+                using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    payload = readStream.ReadToEnd();
+                }
+            }
+            TrafficPackageModel tpm = new TrafficPackageModel()
+            {
+                Path = path,
+                QueryString = queryString,
+                Payload = payload,
+            };
+            HttpResponseMessage response = await client.PostAsJsonAsync("v1/api/TrafficPakages", tpm);
+            string message = await response.Content.ReadAsStringAsync();
+            EventLog log = new EventLog();
+            log.Source = "Application";
+            log.WriteEntry(message);
+        }
     }
 }
---
+++ WebModule/WebModule/Models/TrafficPackageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebModule.Models
+{
+    public class TrafficPackageModel
+    {
+        public string Path { get; set; }
+        public string QueryString { get; set; }
+        public string Payload { get; set; }
+    }
+}
